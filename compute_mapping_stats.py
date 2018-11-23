3b21e4ecaacba009f7c70e7332367bdf0ea072b7
luolab <raywong.chn@gmail.com>
2018-11-23 12:28:53 +0800

    modified variable names in gunzip_counts.py add_suffix.py now adds suffices to counts.tsv and outputs counts_suffix.tsv more suffices to be added on whitelist.txt, whitelist.txt.mapped, etc

--- compute_mapping_stats.py
+++ compute_mapping_stats.py
@@ -1,11 +1,11 @@
-import sys
+import os, re
 import pandas as pd
-import numpy as np
+
 
 whitelist = pd.read_csv('whitelist.txt', sep="\t", header=None)
 
 whitelist.columns = ["cell", "candidate", "cell_counts", "candidate_counts"]
 
 
-
-def if __name__ == '__main__':
+# Not sure how to use this yet. Deal later.
+# def if __name__ == '__main__':
--- rename_gene_id.py
+++ rename_gene_id.py
@@ -1,4 +1,4 @@
-import os
+import os, re
 import pandas as pd
 
 
@@ -7,7 +7,39 @@ import pandas as pd
 parent_wd = '/media/luolab/ZA1BT1ER/yanting/'
 data_wd = '/media/luolab/ZA1BT1ER/yanting/vM4_def/'
 
+nametable = pd.read_table(os.path.join(parent_wd, 'gencode.vM4.annotation.tab'), sep="\t")
 
-nametable = pd.read_table(os.path.join(parent_wd,'gencode.vM4.annotation.tab'), sep="\t")
+nametable = nametable.rename(columns = {'Unnamed: 0': 'gene_id', 'Unnamed: 1': 'gene_name'})
 
-nametable.rename(columns = {'Unnamed: 0': 'gene_id', 'Unnamed: 1': 'gene_name'})
+# Copied from stackOverflow. Works. Read later.
+dictionary = nametable.set_index('gene_id')['gene_name'].T.to_dict()
+
+# Iteratively enter each data folder
+for folder in os.listdir(data_wd):
+
+    # Change working directory
+    os.chdir(os.path.join(data_wd, folder))
+
+    # Extract names parts
+    match = re.search('^([^_]*)_([^_]*)_([^_]*)_([^_]*)_vM4_def$', folder)
+
+    nameparts = [match.group(1), match.group(2), match.group(3), match.group(4), 'counts', 'suffix.tsv']
+
+    filename = '_'.join(nameparts)
+
+    counts_suffix = pd.read_csv(filename, sep='\t')
+
+    # df.replace is so slow for large dataframe
+    # counts.replace({'gene': dictionary})
+
+    # map() function is much more efficient
+    mapped = counts_suffix['gene'].map(dictionary)
+    counts_suffix.gene = mapped
+
+
+    # Output
+    out_nameparts = [match.group(1), match.group(2), match.group(3), match.group(4), 'suffix', 'renamed.tsv']
+    out_filename = '_'.join(out_nameparts)
+    counts_suffix.to_csv(out_filename, sep="\t", index=False)
+
+
