74f3b0ca5e33f29bc56a116de4afe7fbd9b3eac7
luolab <raywong.chn@gmail.com>
2018-11-28 22:43:03 +0800

    Attempting to make everything parallel.

--- wash_whitelist(obsolete).py
+++ wash_whitelist(obsolete).py
@@ -6,10 +6,11 @@ parent_wd = '/media/luolab/ZA1BT1ER/yanting/'
 path_to_mapping_directory = '/media/luolab/ZA1BT1ER/yanting/vM4_def_2/'
 
 # Read ground truth barcode list
-barcode_ground_truth_raw = pd.read_excel(os.path.join(parent_wd,'barcode_ground_truth_checklist.xlsx'))
+barcode_ground_truth_raw = pd.read_excel(os.path.join(parent_wd, 'barcode_ground_truth_checklist.xlsx'))
 
 # Extract ground truth barcodes
-barcode_ground_truth = barcode_ground_truth_raw['Primer_sequence'].str.extract(r'TCAGACGTGTGCTCTTCCGATCT([ATCG]{8})', expand=False)
+barcode_ground_truth = barcode_ground_truth_raw['Primer_sequence'].str.extract(r'TCAGACGTGTGCTCTTCCGATCT([ATCG]{8})',
+                                                                               expand=False)
 
 for folder in os.listdir(path_to_mapping_directory):
 
