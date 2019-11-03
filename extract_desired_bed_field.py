dac8bdd8210ab3e14d9db5e005c6f4bf8a673aea
Hyacinthviolet <raywong.chn@gmail.com>
2019-11-03 16:08:21 +0800

    Added fix_bedfiles.py In the YT..._closest.bed output, tail of the file contain gibberish lines. This code removes those lines based on line numbers in YT.._stranded_nonoverlap.bam.

---
+++ extract_desired_bed_field.py
@@ -0,0 +1,54 @@
+import os
+import re
+# import logging
+import multiprocessing as mp
+import subprocess
+
+
+def work(cmd):
+    lib = re.search('(YT[0-9]*)', cmd).group(1)
+    # Display progress
+    print('Extracting library: ' + lib)
+    return subprocess.call(cmd, shell=True)
+
+
+def awk_extract(libs):
+    cmd_awk_all = []
+    for l in libs:
+
+        # Setting up input/output directory
+        wd = os.path.join(parent_dir, l)
+
+        # Grab folder name prefix
+        match = re.search('^([^_]*)_([^_]*)_([^_]*)_([^_]*)$', l)
+        prefix = match.group(1)
+
+        # Input file name
+        filename_fixed_bed = '_'.join([prefix, 'fixed', 'closest.bed'])
+        path_to_input = os.path.join(wd, filename_fixed_bed)
+
+        # Output file name
+        filename_output = '_'.join([prefix, 'extracted', 'part.bed'])
+        path_to_output = os.path.join(wd, filename_output)
+
+        # Check if output already exists. If not, construct command.
+        if not os.path.exists(path_to_output):
+            # Construct command
+            cmd_this_awk = 'awk \'BEGIN{FS=\"\\t\"; OFS=\"\\t\"}{print $1,$2,$3,$4,$14,$15,$16,$23}\' ' + \
+                           path_to_input + ' > ' + path_to_output
+            cmd_awk_all.append(cmd_this_awk)
+
+    # Parallel run by Pool
+    pool = mp.Pool(1)
+    pool.map(work, cmd_awk_all)
+    print('awk extract desired fields: finished.')
+
+
+def main():
+    parent_dir = '/media/luolab/ZA1BT1ER/yanting/vM23/mapping/'
+    libs = sorted(os.listdir(parent_dir))
+    awk_extract(libs)
+
+
+if __name__ == '__main__':
+    main()
--- readme.txt
+++ readme.txt
@@ -15,3 +15,27 @@ scRNAseq reads mapping pipeline: modified based on umi_tools protocol
 # R pipeline starts here
 
 7. rename_gene_id.py
+
+
+
+
+Fix annotation pipeline:
+
+1. Prepare fixed gtf and bed annotations:
+   a. for gtf: remove non-primary assembly entries (first column is not chrX, i.e. KZ28...)
+      awk -F "\t" 'BEGIN{OFS="\t"} $1~"chr"{print $0}' gencode.vM23.chr_hapl_scaffold.annotation.gtf \
+      > gencode.vM23.chr.annotation.gtf
+   b. for bed: convert gencode_vM23.chr.annotation.gtf with BEDOPS gtf2bed (this also performs sorting, which is
+      required by 'bedtools closest' operation)
+      - (BEDOPS gtf2bed checks GTF column 9 and requires BOTH gene_id and transcript_id entries, which is not satisfied
+      by original gencode gtf files. Generate a fixed file by adding placeholders: transcript_id "";.
+      See this thread: www.biostars.org/p/206342)
+      awk '{ if ($0 ~ "transcript_id") print $0; else print $0" transcript_id \"\";"; }' gencode.vM23.chr.annotation.gtf
+      > gencode.vM23.chr.annotation.fixed.gtf
+      - Now convert with gtf2bed:
+      gtf2bed < gencode.vM23.chr.annotation.fixed.gtf > gencode.vM23.chr.annotation.fixed.bed
+
+2. Run pipe: bedtools_intersect_closest.py
+
+3. Fix bed file errors: fix_bedfiles.py TODO: code to count lines -> num_lines.csv
+
