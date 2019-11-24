dfa650ee2f992e10732aed23555de1188c7f864f
luolab <raywong.chn@gmail.com>
2019-11-24 16:19:52 +0800

    Polishing mapping pipe new

--- mapping_pipeline_parallel_new.py
+++ mapping_pipeline_parallel_new.py
@@ -290,16 +290,16 @@ def main():
     # do_parallel(src_dir=src_dir, dst_dir=dst_dir, task="featurecounts", genome_gtf=genome_gtf, num_thread=32)
 
     # STEP 6: samtools sort
-    do_parallel(src_dir=src_dir, dst_dir=dst_dir, task="samtools_sort", num_process=32)
+    # do_parallel(src_dir=src_dir, dst_dir=dst_dir, task="samtools_sort", num_process=24)
 
     # STEP 7: samtools index
-    # do_parallel(src_dir=src_dir, dst_dir=dst_dir, task="samtools_index", num_process=32)
+    # do_parallel(src_dir=src_dir, dst_dir=dst_dir, task="samtools_index", num_process=16)
 
     # STEP 8: umitools count
-    # do_parallel(src_dir=src_dir, dst_dir=dst_dir, task="umitools_count", num_process=32)
+    do_parallel(src_dir=src_dir, dst_dir=dst_dir, task="umitools_count", num_process=16)
 
     # STEP 9: N unique mapped
-    # do_parallel(src_dir=src_dir, dst_dir=dst_dir, task="nuniquemapped", num_process=32)
+    do_parallel(src_dir=src_dir, dst_dir=dst_dir, task="nuniquemapped", num_process=16)
 
 
 if __name__ == '__main__':
