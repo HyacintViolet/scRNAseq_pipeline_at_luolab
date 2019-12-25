f94564d6c15f23e9f609f363e7404961fd78848f
Hyacinthviolet <raywong.chn@gmail.com>
2019-12-25 23:32:31 +0800

    run mapping_pipeline_parallel_new.py umitools_count

--- mapping_pipeline_parallel_new.py
+++ mapping_pipeline_parallel_new.py
@@ -432,7 +432,7 @@ def main():
     # STEP 10: samtools index
     # do_parallel(src_dir=src_dir2, dst_dir=dst_dir, task="samtools_index", num_process=32)
 
-    STEP 11: umitools count
+    # STEP 11: umitools count
     do_parallel(src_dir=src_dir2, dst_dir=dst_dir, task="umitools_count", num_process=24, overwrite=False)
 
     # OTHER FUNCTIONALITIES
