75819941c81a8a160cc66e45068b584648df65fa
luolab <raywong.chn@gmail.com>
2019-12-26 11:00:30 +0800

    merge

--- mapping_pipeline_parallel_new.py
+++ mapping_pipeline_parallel_new.py
@@ -430,7 +430,7 @@ def main():
     # do_parallel(src_dir=src_dir2, dst_dir=dst_dir, task="samtools_sort", num_thread=32)
 
     # STEP 10: samtools index
-    do_parallel(src_dir=src_dir2, dst_dir=dst_dir, task="samtools_index", num_process=32)
+    # do_parallel(src_dir=src_dir2, dst_dir=dst_dir, task="samtools_index", num_process=32)
 
     # STEP 11: umitools count
     do_parallel(src_dir=src_dir2, dst_dir=dst_dir, task="umitools_count", num_process=24, overwrite=False)
